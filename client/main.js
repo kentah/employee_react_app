37322223ad4ccf29216c1a668b238cfb6d87dbda
kentah <kenthow@gmail.com>
2017-02-11 15:43:23 -0600

    Final commit

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import EmployeeList from './components/EmployeeList';
+
 
 const App = () => {
   return (
-    <div>Hello there!</div>
+    <EmployeeList />
   );
 };
 
